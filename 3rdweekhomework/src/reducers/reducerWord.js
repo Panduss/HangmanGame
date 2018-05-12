633cf27f0174fa91c048154834d504b234658279
Zsuzsi Hudacsko <zsuzsi.hudacsko@gmail.com>
2018-05-12 08:59:33 +0200

    reducer returns now a randomw word, but 3 times after each other. Anyway at least it does return now a random word

--- 3rdweekhomework/src/reducers/reducerWord.js
+++ 3rdweekhomework/src/reducers/reducerWord.js
@@ -1,6 +1,6 @@
 import WordList from '../lib/words'
 
 export default (state = WordList, { type, payload } = {}) => {
-  console.log(state)
-  return state
+  console.log(WordList[Math.floor(Math.random() * WordList.length)])
+  return state;
 }
