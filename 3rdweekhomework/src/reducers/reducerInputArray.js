65f5c7fa1c33dc9acd509700d05ea9fc61c03902
Zsuzsi Hudacsko <zsuzsi.hudacsko@gmail.com>
2018-05-13 09:53:45 +0200

    now if I fire new game action, the input array get's set back to it's initial state (empty)

--- 3rdweekhomework/src/reducers/reducerInputArray.js
+++ 3rdweekhomework/src/reducers/reducerInputArray.js
@@ -2,6 +2,8 @@ export default (state = '', action) => {
   switch(action.type) {
     case 'MAKE_GUESS' :
       return action.payload;
+      case 'NEW_GAME' :
+      return state = '';
     default :
       return state;
     }
